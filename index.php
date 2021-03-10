3eb16f6a84c4f15f6fa9fa7bd2f7ac5a530631d5
Vladimir <vladimir.solomka@gmail.com>
2021-03-10 22:03:05 +0200

    Lesson 03: Homework - Fixes after CR

--- index.php
+++ index.php
@@ -25,37 +25,45 @@ function getSumOf2nds($input)
 {
     $index = 0;
     $sum = 0;
+
     foreach ($input as $value) {
         if (is_array($value)) {
             $sum += getSumOf2nds($value);
-        }
-        if ($index === 1) {
+        } elseif ($index === 1) {
             $sum += intval($value);
         }
         $index++;
     }
+
     return $sum;
 }
 
 echo "Сумма вторых элементов: ";
-echo getSumOf2nds($array0); // 705
+echo getSumOf2nds($array0); // 704
 
 /*
  * 2. Определить количество символов входящих в произвольную строку,
  * т.е у строки 'afrae' a=2, e=1, f=1, r=1
  * */
 
-function getCharCounters($input, $mb = false)
+function getCharCounters($input)
 {
     $output = [];
-    $chars = $mb? mb_str_split($input): str_split($input);
+
+    if (mb_strlen($input) < strlen($input)) {
+        $chars = mb_str_split($input);
+    } else {
+        $chars = str_split($input);
+    }
+
     foreach ($chars as $key => $char) {
         $output[$char]++;
     }
+
     return $output;
 }
 
 echo PHP_EOL;
 print_r(getCharCounters("afrae"));
 echo PHP_EOL;
-print_r(getCharCounters("юfraю", true));
+print_r(getCharCounters("юfraю"));
