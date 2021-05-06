090aec32b6a9a853c3c84724038a3a1fa7977236
Vladimir <vladimir.solomka@gmail.com>
2021-05-06 12:42:42 +0300

    JSON to XML adapter: remake

--- JSON-XML/DataAdapter.php
+++ JSON-XML/DataAdapter.php
@@ -4,41 +4,42 @@ namespace Adapters;
 
 class DataAdapter
 {
-    public const XML_ROOT_NODE = 'root';
-    public const XML_DEFAULT_NODE_NAME = 'node';
+    public const XML_ROOT_NODE = '<root/>';
+    private const PRIMITIVE_TYPES = ['string', 'number', 'boolean', 'null'];
 
     public static function jsonAsXML($json)
     {
-        $xml = new \SimpleXMLElement('<' . self::XML_ROOT_NODE . '/>');
+        $xml = new \SimpleXMLElement(self::XML_ROOT_NODE);
         self::insertNodeXML($xml, 0, json_decode($json));
         return $xml->asXML();
     }
 
     private static function insertNodeXML(&$parent, $key, $element)
     {
-        if (in_array(gettype($element), ["array", "object"])) {
-            $node = $parent->addChild(self::xmlNodeName($key), "");
-            if (gettype($element) === "object") {
-                $properties = get_object_vars($element);
-                foreach ($properties as $key => $value) {
-                    if (!in_array(gettype($value), ["array", "object"])) {
-                        $node->addAttribute($key, $value);
-                    } else {
-                        self::insertNodeXML($node, $key, $value);
-                    }
-                }
-            } elseif (gettype($element) === "array") {
-                foreach ($element as $key => $value) {
-                    self::insertNodeXML($node, $key, $value);
-                }
-            }
-        } else {
+        if (in_array(gettype($element), self::PRIMITIVE_TYPES)) {
             $parent->addChild(self::xmlNodeName($key), $element);
+        }
+
+        if (is_array($element)) {
+            foreach ($element as $value) {
+                self::insertNodeXML($parent, self::xmlNodeName($key), $value);
+            }
+        }
+
+        if (is_object($element)) {
+            if (!is_numeric($key)) {
+                $xml = $parent->addChild(self::xmlNodeName($key), null);
+            } else {
+                $xml = $parent;
+            }
+            foreach (get_object_vars($element) as $property_key => $property_value) {
+                self::insertNodeXML($xml, self::xmlNodeName($property_key), $property_value);
+            }
         }
     }
 
     private static function xmlNodeName($key)
     {
-        return (gettype($key) === 'integer') ? self::XML_DEFAULT_NODE_NAME : $key;
+        return preg_replace('/[\W]+/', '_', $key);
     }
 }
